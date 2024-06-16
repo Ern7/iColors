0786064760f55aeeabfa5ce242e1cf87abbd58ec
Ernest Nyumbu <ernestnyumbu@gmail.com>
2024-06-16 19:22:15 +0200

    feat (ColorExtensions) : Implemented Color extenstions.

---
+++ Sources/iColors/ColorExtensions.swift
@@ -0,0 +1,60 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ernest Nyumbu on 2024/06/16.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+public extension Color {
+    /// Convert hex string to Color
+    /// - Parameter hex: Hex color code as a string
+    init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
+
+public extension UIColor {
+    /// Convert hex string to Color
+    /// - Parameter hex: Hex color code as a string
+    convenience init(hex: String) {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if (cString.count) != 6 {
+            self.init(
+                red: CGFloat(200) / 255.0,
+                green: CGFloat(200) / 255.0,
+                blue: CGFloat(200) / 255.0,
+                alpha: CGFloat(1.0)
+            )
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
